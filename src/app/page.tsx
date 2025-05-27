109e917c58426d6df6c5b37176771b3d62400102
Bernardo Filipe <dev.bernardofofg@gmail.com>
2025-05-27 01:21:51 -0300

    feat: add new dependencies for UI components and animations; update global styles and page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-screen w-full bg-neutral-950 text-accent">
       <h1>Hello World</h1>
+      <Button>Bootcamp FSW</Button>
     </div>
   );
 }
