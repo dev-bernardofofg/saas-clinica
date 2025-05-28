5a1c2bbbeada4a70a53aa0c9f825137b102cc0c4
Bernardo Filipe <dev.bernardofofg@gmail.com>
2025-05-28 01:16:10 -0300

    feat: update dashboard page to display user email and implement sign-out functionality; enhance BaseButton for action handling

--- src/app/(routes)/(private)/dashboard/page.tsx
+++ src/app/(routes)/(private)/dashboard/page.tsx
@@ -1,5 +1,19 @@
-const Dashboard = () => {
-  return <div>Dashboard</div>;
+import { BaseButton } from "@/components/(bases)/base-button";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  return (
+    <div>
+      DashboardPage
+      <p>{session?.user?.email}</p>
+      <BaseButton clickAction="sign-out">Sair</BaseButton>
+    </div>
+  );
 };
 
-export default Dashboard;
+export default DashboardPage;
--- src/components/(bases)/base-button.tsx
+++ src/components/(bases)/base-button.tsx
@@ -4,11 +4,14 @@ import { Loader2 } from "lucide-react";
 import { ComponentProps } from "react";
 
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
 interface BaseButtonProps extends ComponentProps<typeof Button> {
   isLoading?: boolean;
   loadingText?: string;
+  clickAction?: "default" | "sign-out";
 }
 
 export const BaseButton = ({
@@ -17,12 +20,25 @@ export const BaseButton = ({
   loadingText,
   className,
   disabled,
+  clickAction = "default",
   ...props
 }: BaseButtonProps) => {
+  const { push } = useRouter();
   return (
     <Button
       className={cn("w-full", className)}
       disabled={disabled || isLoading}
+      onClick={async () => {
+        if (clickAction === "sign-out") {
+          await authClient.signOut({
+            fetchOptions: {
+              onSuccess: () => {
+                push("/");
+              },
+            },
+          });
+        }
+      }}
       {...props}
     >
       {isLoading ? (
